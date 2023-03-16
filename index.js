75034b546cf6784fc510773a717d528b8bc2b28d
Jordi Jerico <jordijerico@gmail.com>
2023-03-16 13:47:45 +0100

    cors added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 const express = require('express');
 const db = require("./db");
-
+const cors = require('cors');
 const app = express();
 //Importo el método de conexion a la base de datos
 const authRoutes = require('./views/authRoutes');
 const userRoutes = require('./views/userRoutes');
 const appointmentRoutes = require('./views/appointmentRoutes');
 
-
+app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
 app.use(userRoutes);
