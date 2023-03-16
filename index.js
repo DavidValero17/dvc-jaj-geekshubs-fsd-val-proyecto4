989d4e6c595eba36ab7d9ad2253524d6009155cc
Jordi Jerico <jordijerico@gmail.com>
2023-03-16 13:55:57 +0100

    cors added

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-
+const cors = require('cors');
 const express = require('express');
 const db = require("./db");
-const cors = require('cors');
 const app = express();
+const router = require('./router');
 //Importo el método de conexion a la base de datos
-const authRoutes = require('./views/authRoutes');
-const userRoutes = require('./views/userRoutes');
-const appointmentRoutes = require('./views/appointmentRoutes');
 
+let corsOptions = {//CONFIGURO OPCIONES DE CORS
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors());
 
 app.use(express.json());
-app.use(router)
+app.use(router);
+
 
 
 const PORT = 4000;
