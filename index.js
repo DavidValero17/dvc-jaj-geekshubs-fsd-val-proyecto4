b9868abd8722478c51daf4c869f5fc41a11f7144
David Valero <davidvalero17@hotmail.com>
2023-03-23 16:09:16 +0100

    added cors

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const cors = require('cors');
 const express = require('express');
 const cors = require("cors");
 const db = require("./db");
 const app = express();
 const router = require('./router');
 //Importo el método de conexion a la base de datos
-
-let corsOptions = {//CONFIGURO OPCIONES DE CORS
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-};
 
 app.use(cors());
 let corsOptions = {
