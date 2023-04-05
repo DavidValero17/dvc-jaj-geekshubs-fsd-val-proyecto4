f69ad7ce505c765fad99b4a51e54ec2640c829b1
David Valero <davidvalero17@hotmail.com>
2023-04-05 11:05:27 +0200

    Merge branch 'master' of https://github.com/DavidValero17/dvc-jaj-geekshubs-fsd-val-proyecto4

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(router)
 
 
 
-const PORT = 4000;
+const PORT =  process.env.PORT;
 
 db.then(() => {
     app.listen(PORT, () => console.log("Server on port " + PORT));
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -12,7 +12,7 @@ router.get('/profile', verifyToken, userController.getProfile);
 router.put('/profile/update', verifyToken, userController.updateProfile);
 router.get('/profile/checkallclients', verifyToken, isAdmin, userController.getAllClients);
 router.get('/appointments/checkall', verifyToken, isClient, userController.getAppointmentsByUser);
-router.get('/appointments/checkall/doctor', verifyToken, isDoctor, userController.getAllAppointments);
+router.get('/appointments/checkall/admin', verifyToken, isAdmin, userController.getAllAppointments);
 
 
 
